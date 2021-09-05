4beced22448b9994269c15a2fcb40a4fcdd81152
alex.wilson <alex.wilson@forgerock.com>
2021-09-05 21:08:48 +0100

    Working but only when out of stock

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"strings"
+
 	"github.com/gocolly/colly"
 )
 
@@ -13,18 +15,16 @@ func main() {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	c.OnHTML("span.a-size-medium.a-color-price", func(e *colly.HTMLElement) {
-		// Goquery selection of the HTMLElement is in e.DOM
-		//goquerySelection := e.DOM
+	c.OnHTML("div[id=availability]", func(e *colly.HTMLElement) {
 
-		// Example Goquery usage
-		//fmt.Println(e)
-		ch := e.DOM.Text()
-		fmt.Printf("test:%s", ch)
+		stock := strings.Split(e.ChildText("span.a-size-medium.a-color-price"), "\n")[0]
+		fmt.Printf("test:%s\n", stock)
+
 	})
 
 	c.Visit("https://www.amazon.co.uk/PlayStation-9395003-5-Console/dp/B08H95Y452/ref=sr_1_3?dchild=1&keywords=ps5&qid=1630621564&sr=8-3")
 
-	//c.Visit("https://www.amazon.co.uk/Xbox-RRT-00007-Series-X/dp/B08H93GKNJ")
+	c.Visit("https://www.amazon.co.uk/Xbox-RRT-00007-Series-X/dp/B08H93GKNJ")
+	c.Visit("https://www.amazon.co.uk/Konsole-Sony-Playstation-D-Chasis-Controller/dp/B01LQF9RDI/ref=sr_1_1?dchild=1&keywords=ps4&qid=1630872165&sr=8-1")
 
 }
